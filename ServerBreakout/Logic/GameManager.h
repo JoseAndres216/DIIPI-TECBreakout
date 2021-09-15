bbc79efd60bda59139c4583e9b15a5d288327327
Jose Andres <joseandres216@estudiantec.cr>
2021-09-14 18:29:14 -0600

    Auto stash before merge of "main" and "origin/main"
    Generated base matrix

--- ServerBreakout/Logic/GameManager.h
+++ ServerBreakout/Logic/GameManager.h
@@ -21,7 +21,7 @@ private:
 
 
     GameManager() {
-        //Asignacion de valores
+        //No se para que sirve el constructor con Singleton
     }
 
 public:
@@ -33,8 +33,36 @@ public:
         return instance;
     }
 
+    Matrix getMatrix(){
+        return matrix;
+    }
+
+    void setMatrix(Matrix matrix) {
+        GameManager::matrix = matrix;
+    }
+
     void generateMatrix() {
-        cout << "Se genera la matriz!";
+        int y = 100;
+        for(int i = 1; i<=6; i++){
+            matrix.addRow();
+            if(i == 1 or i == 2){
+                for(int j = 1; j<=14; j++){
+                    matrix.addElement(i, (j*100), y, 3);
+                }
+                y += 50;
+            }else if(i == 3 or i == 4){
+                for(int j = 1; j<=14; j++){
+                    matrix.addElement(i, (j*100), y, 2);
+                }
+                y += 50;
+            }else{
+                for(int j = 1; j<=14; j++){
+                    matrix.addElement(i, (j*100), y, 1);
+                }
+                y += 50;
+            }
+        }
+
     }
 };
 
